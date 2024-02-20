ef165d2c4233577a941c290c146e1b4960e82632
Vasya <boyko1396@gmail.com>
2024-02-20 14:31:30 +0200

    fix 2 page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-    $('.header__toggle').click(function() {
-        $('body').toggleClass('is-show');
+  // header toggle
+  $('.header__toggle').click(function() {
+    $('body').toggleClass('is-show');
+  });
+
+  // document card toggle
+  if ($('.documentation__card').length > 0) {
+    $('.documentation__card:first').addClass('is-show');
+    $('.documentation__card:first').find('.documentation__card-dropdown').show();
+  }
+
+  if ($('.js-doc-card-toggle').length > 0) {
+    $('.js-doc-card-toggle').click(function() {
+      $(this).parent().toggleClass('is-show');
+      $(this).siblings().slideToggle(450);
     });
+  }
 });
